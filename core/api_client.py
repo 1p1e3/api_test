e27bbc81f606fe364ef96bae52f5cfc89107bbb5
kiramika <2762851151@qq.com>
2026-01-22 21:15:37 +0800

    :sparkles:feat:implement robust APIClient with retry and session management

---
+++ core/api_client.py
@@ -0,0 +1,48 @@
+from typing import Dict, Optional
+from requests import Session
+from config.settings import settings
+from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
+
+class APIClient:
+    def __init__(self,
+                 base_url: Optional[str] = None,
+                 extra_header: Optional[Dict[str, str]] = None,
+                 timeout: int = 10,
+                 max_retries: int = 3,
+                 ):
+        
+        # 优先使用实例化时显式传入的 base_url
+        self.base_url = base_url or settings.API_BASE_URL
+        
+        # 
+        self.session = Session()
+        # 设置请求 header
+        self.session.headers.update({
+            'Content-Type': 'application/json',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36',
+            **(extra_header or {})
+        })
+
+
+        # 重试策略
+        retry_strategy = Retry(
+            total=max_retries,
+            backoff_factor=1,
+            status_forcelist=[429, 500, 502, 503, 504],
+            allowed_methods=["HEAD", "GET", "OPTIONS", "POST", "PUT", "DELETE"]
+        )
+
+
+        # 配置适配器
+        adapter = HTTPAdapter(max_retries=retry_strategy)
+        self.session.mount("http://", adapter)
+        self.session.mount("https://", adapter)
+
+
+        # token
+        self.token = Optional[str] = None
+
+
+        
+
