f860a36f86b5b69137957b513ac1e86fb56e2494
kiramika <2762851151@qq.com>
2026-02-03 21:47:53 +0800

    :sparkles:closed_lock_with_key: add auth_token fixture for API authentication

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+from config.settings import settings
 from core.api_client import APIClient
 
 
@@ -8,5 +9,20 @@ def api_client():
     """无认证的通用客户端"""
     return APIClient()
 
+
+@pytest.fixture(scope='session')
+def auth_token():
+    """
+    登录获取 token
+    """
+    client = APIClient()
+    resp = client.request('POST', f'login', json={
+        'username': settings.USERNAME,
+        'password': settings.PASSWORD
+    })
+
+    token = resp.json()['data']['token']
+
+    return token
 
 
