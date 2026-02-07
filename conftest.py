ebda33b15cb8cb238f3e9578780ac93f6fc098bc
kiramika <2762851151@qq.com>
2026-02-07 19:38:31 +0800

    :sparkles:conftest: implement db fixture and session db pool cleanup hook

--- conftest.py
+++ conftest.py
@@ -2,14 +2,13 @@ import pytest
 
 from config.settings import settings
 from core.api_client import APIClient
+from core.mysql_client import MySQLClient
 
 
 @pytest.fixture(scope='session')
-def api_client():
+def unauthorized_client():
     """无认证的通用客户端"""
     return APIClient()
-
-
 
 
 @pytest.fixture(scope='session')
@@ -31,4 +30,20 @@ def authorized_client(api_client):
 
     return api_client
 
+
+
+# 利用 pytest 的 fixture 特性，实现数据库连接的获取
+# 利用 pytest 生命周期钩子函数，实现数据库连接池的关闭
+@pytest.fixture(scope='function')
+def db():
+    """函数级别的 fixture, 每个测试用例获取一个独立的数据库连接
+    """
+    with MySQLClient() as client:
+        yield client
+
+def pytest_sessionfinish(session, exitstatus):
+    """测试结束后操作
+    """
+    # 关闭 MySQL 连接池
+    MySQLClient.close_pool()
 
