85115c545c9d262bc2fd92be18ce8b706081032e
kiramika <2762851151@qq.com>
2026-02-19 21:59:00 +0800

    :rocket:feat(config): add project directory to paths.py

--- config/paths.py
+++ config/paths.py
@@ -6,3 +6,16 @@ ROOT_DIR = Path().resolve()
 CONFIG_DIR = ROOT_DIR.joinpath('config')
 
 LOGS_DIR = ROOT_DIR.joinpath('logs')
+
+CORE_DIR = ROOT_DIR.joinpath('core')
+
+MODELS_DIR = ROOT_DIR.joinpath('models')
+
+DB_MODELS_DIR = MODELS_DIR.joinpath('db_models')
+
+RESPONSE_MODELS_DIR = MODELS_DIR.joinpath('response_models')
+
+UTILS_DIR = ROOT_DIR.joinpath('utils')
+
+# 测试用例
+CASES_DIR = ROOT_DIR.joinpath('cases')
